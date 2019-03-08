fbcfea33296e3d772f646544f769504ae7ca9fc1
shuoyangd <shuoyangd@gmail.com>
2019-03-08 15:44:58 -0500

    try torch gather instead of advidx

--- model/StackLSTMCell.py
+++ model/StackLSTMCell.py
@@ -3,7 +3,6 @@ import torch
 from torch import nn
 # from torch.autograd import Variable
 
-
 class StackLSTMCell(nn.Module):
 
   def __init__(self, input_size, hidden_size, dropout_rate, stack_size, num_layers=1,
@@ -69,15 +68,21 @@ class StackLSTMCell(nn.Module):
 
     batch_size = input.size(0)
     batch_indexes = torch.arange(0, batch_size).type(self.long_dtype)
-    cur_hidden, cur_cell = self.hidden_stack[self.pos, batch_indexes, :, :], \
-                           self.cell_stack[self.pos, batch_indexes, :, :]
+    pos = self.pos.unsqueeze(0).unsqueeze(2).unsqueeze(3).expand(1, batch_size, self.hidden_size, self.num_layers)
+    cur_hidden = torch.gather(self.hidden_stack, 0, pos)
+    cur_cell = torch.gather(self.cell_stack, 0, pos)
+    # cur_hidden, cur_cell = self.hidden_stack[self.pos, batch_indexes, :, :], \
+    #                        self.cell_stack[self.pos, batch_indexes, :, :]
     next_hidden, next_cell = self.lstm(input, (cur_hidden, cur_cell))
     self.hidden_stack[self.pos + 1, batch_indexes, :, :] = next_hidden.clone()
     self.cell_stack[self.pos + 1, batch_indexes, :, :] = next_cell.clone()
     self.pos = self.pos + op  # XXX: should NOT use in-place assignment!
-
-    hidden_ret = self.hidden_stack[self.pos, batch_indexes, :, :]
-    cell_ret = self.cell_stack[self.pos, batch_indexes, :, :]
+    
+    # hidden_ret = self.hidden_stack[self.pos, batch_indexes, :, :]
+    # cell_ret = self.cell_stack[self.pos, batch_indexes, :, :]
+    pos = self.pos.unsqueeze(0).unsqueeze(2).unsqueeze(3).expand(1, batch_size, self.hidden_size, self.num_layers)
+    hidden_ret = torch.gather(self.hidden_stack, 0, pos)
+    cell_ret = torch.gather(self.cell_stack, 0, pos)
     return hidden_ret[:, :, -1], cell_ret[:, :, -1]
 
   def init_hidden(self, init_var=None):
@@ -98,46 +103,6 @@ class StackLSTMCell(nn.Module):
 
   def size(self):
     return torch.max(self.pos + 1)[0].item()
-
-
-class ReducerLSTMCell(StackLSTMCell):
-
-  def forward(self, input, op):
-    """
-    stack needs to be built before this is called.
-
-    :param input: (batch_size, input_size), input vector, in batch.
-    :param op: (batch_size,), stack operations, in batch (-1 means reduce, +1 means push).
-    :return: (hidden, cell): both are (batch_size, hidden_dim)
-    """
-
-    batch_size = input.size(0)
-    batch_indexes = torch.arange(0, batch_size).type(self.long_dtype)
-
-    # take care of the shifts
-    cur_hidden, cur_cell = self.hidden_stack[self.pos, batch_indexes, :, :], \
-                           self.cell_stack[self.pos, batch_indexes, :, :]
-    next_hidden, next_cell = self.lstm(input, (cur_hidden, cur_cell))
-
-    self.hidden_stack[self.pos + 1, batch_indexes, :, :] = next_hidden.clone()
-    self.cell_stack[self.pos + 1, batch_indexes, :, :] = next_cell.clone()
-
-    # take care of the reduces
-    reduced_indexes = batch_indexes[op == -1]
-    reduced_pos = (self.pos - 1)[op == -1]
-    if len(reduced_indexes) != 0:
-      prev2_hidden, prev2_cell = self.hidden_stack[self.pos - 2, batch_indexes, :, :], \
-                                 self.cell_stack[self.pos - 2, batch_indexes, :, :]
-      next_hidden, next_cell = self.lstm(input, (prev2_hidden, prev2_cell))
-      self.hidden_stack[reduced_pos, reduced_indexes, :, :] = next_hidden[reduced_indexes, :, :]
-      self.cell_stack[reduced_pos, reduced_indexes, :, :] = next_cell[reduced_indexes, :, :]
-
-    self.pos = self.pos + op  # XXX: should NOT use in-place assignment!
-
-    hidden_ret = self.hidden_stack[self.pos, batch_indexes, :, :]
-    cell_ret = self.cell_stack[self.pos, batch_indexes, :, :]
-    return hidden_ret[:, :, -1], cell_ret[:, :, -1]
-
 
 class BatchedToyStackModel(nn.Module):
 
